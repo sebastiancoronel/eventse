f207164e0f0222dd7511f5a2c1969e61f21e190f
sebastiancoronel <seba_c94@hotmail.com>
2020-04-09 00:35:37 -0300

    09/04/2020

--- resources/views/Perfiles/Cliente/Menu/Servicios/servicios_finalizados.blade.php
+++ resources/views/Perfiles/Cliente/Menu/Servicios/servicios_finalizados.blade.php
@@ -1,13 +1,15 @@
 @extends('Perfiles.home')
 @section('content')
 {{-- Escritorio --}}
-<div class="d-none d-sm-block mt-5 p-4" style="padding-top: 5em !important;">
+<div class=" mt-5 p-4" style="padding-top: 5em !important;">
   <h4 class="card-header text-muted">Finalizados</h4>
-  <!-- Card -->
+
+  <div class="p-4 col-md-4">
+
   <div class="card">
     <!-- Card image -->
-    <div class="view overlay">
-      <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo9.jpg" alt="Card image cap">
+    <div class="">
+      <img class="card-img-top" src="{{asset('images/castillo2.webp')}}" alt="Card image cap">
       <a>
         <div class="mask rgba-white-slight"></div>
       </a>
@@ -19,7 +21,7 @@
       <div class="social-reveal">
 
         <!-- Facebook -->
-        <a type="button" class="btn-floating btn-fb mt-0 mx-1"><i class="fab fa-facebook-f"></i></a>
+        <a type="button" class="btn-floating btn-fb mt-0 mx-1"><i class="zmdi zmdi-help"></i></a>
         <!-- Twitter -->
         <a type="button" class="btn-floating btn-tw mt-0 mx-1"><i class="fab fa-twitter"></i></a>
         <!-- Google -->
@@ -28,7 +30,7 @@
       </div>
 
       <!-- Button action -->
-      <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+      <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="zmdi zmdi-plus"></i></a>
 
     </div>
 
@@ -41,11 +43,12 @@
       <!-- Text -->
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
         content.</p>
-      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+        <button class="btn btn-indigo text-white btn-rounded btn-md mt-4">Calificar</button>
 
+      </div>
     </div>
-  </div>
-  <!-- Card -->
+</div>
+
 </div>
 
 
