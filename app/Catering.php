868e54371fb56208244f88748a96a4e95fd63532
sebastiancoronel <seba_c94@hotmail.com>
2020-06-11 17:21:21 -0300

    Modificacion en modelos Catering y MusicaDJ

--- app/Catering.php
+++ app/Catering.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Catering extends Model
 {
+  use SoftDeletes;
   protected $table = 'caterings';
   protected $fillable = ['foto_1','foto_2','foto_3','foto_4','titulo','cantidad_invitados','servicio_pizza',
                         'mesa_dulce','pernil','coffee_break','servicio_lunch','servicio_gourmet','provincia',
--- database/migrations/2020_06_09_234709_create_musica_djs_table.php
+++ database/migrations/2020_06_09_234709_create_musica_djs_table.php
@@ -15,6 +15,35 @@ class CreateMusicaDjsTable extends Migration
     {
         Schema::create('musica_djs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('foto_1');
+            $table->string('foto_2');
+            $table->string('foto_3');
+            $table->string('foto_4');
+            $table->string('titulo');
+            $table->boolean('cumpleaños_infantiles')->nullable();
+            $table->boolean('cumpleaños_adultos')->nullable();
+            $table->boolean('fiestas_tematicas')->nullable();
+            $table->boolean('comuniones')->nullable();
+            $table->boolean('bodas_y_aniversarios')->nullable();
+            $table->boolean('musica_70')->nullable();
+            $table->boolean('musica_80')->nullable();
+            $table->boolean('musica_90')->nullable();
+            $table->boolean('musica_2000')->nullable();
+            $table->boolean('clasicos')->nullable();
+            $table->boolean('cumbia')->nullable();
+            $table->boolean('cuarteto')->nullable();
+            $table->boolean('reggaeton')->nullable();
+            $table->boolean('bachata')->nullable();
+            $table->string('descripcion');
+            $table->string('provincia');
+            $table->string('localidad');
+            $table->integer('precio')->nullable();
+            $table->boolean('precio_a_convenir')->nullable();
+            $table->integer('id_categoria')->unsigned();
+            $table->foreign('id_categoria')->references('id')->on('categorias');
+            $table->integer('id_prestador')->unsigned();
+            $table->foreign('id_prestador')->references('id')->on('prestadors');
+            $table->date('fecha_publicacion');
             $table->timestamps();
         });
     }
