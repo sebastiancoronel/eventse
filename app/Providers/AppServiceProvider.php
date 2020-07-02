a32b8bd8299cc7c3cd2ca29daaa597c5739065ce
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2020-07-02 19:38:39 -0300

    Modif. Crear array con el carito

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,13 @@ use Illuminate\Support\Facades\Route;
 use App\Categoria;
 use App\Cliente;
 use App\Carrito;
+
+use App\Inmueble;
+use App\Juego;
+use App\Animacion;
+use App\Mobiliario;
+use App\Catering;
+use App\MusicaDj;
 use Auth;
 
 class AppServiceProvider extends ServiceProvider
@@ -55,41 +62,55 @@ class AppServiceProvider extends ServiceProvider
                               ->select('*')
                               ->get();
 
-          $ElementosCarrito = array();
+          $ServiciosCarrito = array();
 
           foreach ( $Carrito as $carrito ) {
-            switch ($carrito) {
+            
+            switch ($carrito) {  
               case $carrito->id_inmueble != null:
-                dd('HAY INMUEBLE');
+                $InmuebleAgregado = Inmueble::find($carrito->id_inmueble);
+                array_push( $ServiciosCarrito , $InmuebleAgregado );
+                //dd($InmuebleAgregado);
                 break;
 
               case $carrito->id_juego != null:
-                dd('HAY Juego');
+                $JuegoAgregado = Juego::find($carrito->id_juego);
+                array_push($ServiciosCarrito , $JuegoAgregado );
+                //dd($JuegoAgregado);
                 break;
               
               case $carrito->id_animacion != null:
-                dd('HAY Animacion');
+                $AnimacionAgregado = Animacion::find($carrito->id_animacion);
+                array_push($ServiciosCarrito , $AnimacionAgregado );
+                //dd('HAY Animacion');
                 break;
               
               case $carrito->id_mobiliario != null:
-                dd('HAY Mobiliario');
+                $MobiliarioAgregado = Mobiliario::find($carrito->id_mobiliario);
+                array_push($ServiciosCarrito , $MobiliarioAgregado );
+                //dd('HAY Mobiliario');
                 break;
 
               case $carrito->id_catering != null:
-                dd('HAY Catering');
+                $CateringAgregado = Catering::find($carrito->id_catering);
+                array_push($ServiciosCarrito , $CateringAgregado );
+                //dd('HAY Catering');
                 break;
 
               case $carrito->id_musicaDj != null:
-                dd('HAY MusicaDj');
+                $MusicaDjAgregado = MusicaDj::find($carrito->id_musicaDj);
+                array_push($ServiciosCarrito , $MusicaDjAgregado );
+                //dd('HAY MusicaDj');
                 break;
 
-              default:
-                # code...
+              default: 
+              'El paquete está vacío';
                 break;
             }                              
           }
-                              
-            dd($Carrito);
+          
+            dd($ServiciosCarrito); //Recorrer en la vista barra_nav_princ. con un foreach para listar lo que Está en el carro.
+
             $CantidadServicios = $Carrito->count();
             //dd($CantidadServicios);
                     
