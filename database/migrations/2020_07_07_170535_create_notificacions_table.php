fbf365c489f94a714bc5e12f86c1eb2e47a1a774
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2020-07-14 20:29:22 -0300

    modif tabla de notificaciones

--- database/migrations/2020_07_07_170535_create_notificacions_table.php
+++ database/migrations/2020_07_07_170535_create_notificacions_table.php
@@ -15,8 +15,23 @@ class CreateNotificacionsTable extends Migration
     {
         Schema::create('notificacions', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer(''); COMPLETAR ESTO PARA VER QUIEN MANDA LA NOTIFICACION
-            $table->foreign('');
+            $table->integer('id_cliente');
+            $table->foreign('id_cliente')->references('id')->on('clientes');
+            $table->integer('id_prestador');
+            $table->foreign('id_prestador')->references('id')->on('prestadors');
+
+            $table->integer('id_inmueble');
+            $table->foreign('id_inmueble')->references('id')->on('inmuebles');
+            $table->integer('id_juego');
+            $table->foreign('id_juego')->references('id')->on('juegos');
+            $table->integer('id_animacion');
+            $table->foreign('id_animacion')->references('id')->on('animacions');
+            $table->integer('id_mobiliario');
+            $table->foreign('id_mobiliario')->references('id')->on('mobiliarios');
+            $table->integer('id_catering');
+            $table->foreign('id_catering')->references('id')->on('caterings');
+            $table->integer('id_musicaDj');
+            $table->foreign('id_musicaDj')->references('id')->on('musica_djs');
             $table->enum('tipo_actividad',['Nueva pregunta','Nueva respuesta','Alquiler confirmado']);
             $table->string('URL');
             $table->dateTime('hora_envio');
