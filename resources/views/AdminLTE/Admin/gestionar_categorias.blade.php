edf754e10e9c8eeac8829768de850778f661a020
Sebastian <seba_c94@hotmail.com>
2020-11-06 15:58:59 -0300

    Modif en vista gestionar categorias

--- resources/views/AdminLTE/Admin/gestionar_categorias.blade.php
+++ resources/views/AdminLTE/Admin/gestionar_categorias.blade.php
@@ -1,11 +1,24 @@
-@extends('AdminLTE.home')
-@section('content')
-<div class="dispositivo">
-    <h4 class="card-header text-muted text-uppercase"> Categorías <a class="btn btn-primary pull-right"> + Agregar </a>  </h4>
+@extends('AdminLTE.home')@section('content')
+<div class="dispositivo container">
+    <h4 class="card-header text-muted text-uppercase"> Categorías <a class="btn btn-primary pull-right boton_agregar_categoria"> + Agregar </a>  </h4>
+    
+    {{-- Formulario agregar categoria --}}
+    <div class="card form-agregar-categoria d-none">
+        <div class="card-body">
+            <div class="md-form">
+                <form action=" {{ route('AlmacenarCategoria') }} " method="POST">
+                    <input type="text" class="form-control" name="nombre_categoria" id="nombre_categoria" maxlength="60">
+                    <label for="form1">Nombre de la categoria</label>
+                    <button class="btn btn-primary"> Aceptar </button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    {{-- Tabla de categorias --}}
     <div class="card">
         <div class="card-body">
             <table class="table">
-            
                 <thead>
                     <tr>
                         <th>Nombre</th>
@@ -29,4 +42,12 @@
         </div>
     </div>
 </div>
+
+{{-- HACER UN PUSH A LA VISTA MASTER --}}
+<script>
+    $(document).on('click','.boton_agregar_categoria',function(){
+        $('.form-agregar-categoria').removeClass('d-none');
+        $('.form-agregar-categoria').addClass('d-block');
+    });
+</script>
 @endsection
