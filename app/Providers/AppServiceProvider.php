e938cc21f2f6ed6f59632522fd80e6350a4e8b69
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2020-11-19 19:22:20 -0300

    Se trae al paquete todos los servicios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,15 +7,7 @@ use  Illuminate\Support\Facades\Schema;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Route;
 use App\Categoria;
-use App\Cliente;
-use App\Carrito;
-
-use App\Inmueble;
-use App\Juego;
-use App\Animacion;
-use App\Mobiliario;
-use App\Catering;
-use App\MusicaDj;
+use Session;
 use Auth;
 
 class AppServiceProvider extends ServiceProvider
@@ -38,7 +30,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191); //AGREGUE ESTO POR EL ERROR DE TABLA Al HACER LA MIGRACION DE LA BASE DE DATOS
+        
+        view()->composer('*',function(View $view){
 
+            $Paquete = Session::get('Servicio');
+            //dd($Paquete);
+            if ( $Paquete != null ) {
+                $CantidadServicios = count($Paquete);
+            }else{
+                $CantidadServicios = 0;
+            }
+
+            $view->with('Paquete',$Paquete)->with('CantidadServicios',$CantidadServicios );
+        });
+        
         // view()->composer('Perfiles.home',function(View $view){
         //   //Traer todas las categorias
         //   $categorias = Categoria::all();
@@ -46,78 +51,5 @@ class AppServiceProvider extends ServiceProvider
         //   $view->with('Categorias',$categorias);
         // });
 
-        //view()->composer('layouts.barra_navegacion_principal',function(View $view){
-          //Traer Categorias
-          //$categorias = Categoria::all();
-
-          // if (Auth::user()) {
-          //   //Traer Carrito para cliente logueado
-          //   $user_id = Auth::user()->id;
-
-          //   $Cliente = Cliente::where('user_id', $user_id)
-          //                     ->select('*')
-          //                     ->first();
-          //   //dd($Cliente->id);
-          //   $Carrito = Carrito::where('id_cliente', $Cliente->id)
-          //                     ->select('*')
-          //                     ->get();
-
-          // $ServiciosCarrito = array();
-
-          // foreach ( $Carrito as $carrito ) {
-            
-          //   switch ($carrito) {  
-          //     case $carrito->id_inmueble != null:
-          //       $InmuebleAgregado = Inmueble::find($carrito->id_inmueble);
-          //       array_push( $ServiciosCarrito , $InmuebleAgregado );
-          //       //dd($InmuebleAgregado);
-          //       break;
-
-          //     case $carrito->id_juego != null:
-          //       $JuegoAgregado = Juego::find($carrito->id_juego);
-          //       array_push($ServiciosCarrito , $JuegoAgregado );
-          //       //dd($JuegoAgregado);
-          //       break;
-              
-          //     case $carrito->id_animacion != null:
-          //       $AnimacionAgregado = Animacion::find($carrito->id_animacion);
-          //       array_push($ServiciosCarrito , $AnimacionAgregado );
-          //       //dd('HAY Animacion');
-          //       break;
-              
-          //     case $carrito->id_mobiliario != null:
-          //       $MobiliarioAgregado = Mobiliario::find($carrito->id_mobiliario);
-          //       array_push($ServiciosCarrito , $MobiliarioAgregado );
-          //       //dd('HAY Mobiliario');
-          //       break;
-
-          //     case $carrito->id_catering != null:
-          //       $CateringAgregado = Catering::find($carrito->id_catering);
-          //       array_push($ServiciosCarrito , $CateringAgregado );
-          //       //dd('HAY Catering');
-          //       break;
-
-          //     case $carrito->id_musicaDj != null:
-          //       $MusicaDjAgregado = MusicaDj::find($carrito->id_musicaDj);
-          //       array_push($ServiciosCarrito , $MusicaDjAgregado );
-          //       //dd('HAY MusicaDj');
-          //       break;
-
-          //     default: 
-          //     'El paquete está vacío';
-          //       break;
-          //   }                              
-          // }
-
-          //   $CantidadServicios = $Carrito->count();
-            
-          //    $view->with('Cliente',$Cliente)->with('Carrito',$Carrito)->with('CantidadServicios', $CantidadServicios)->with( 'ServiciosCarrito', $ServiciosCarrito );
-          
-          // }else{
-          //   //$view->with('Categorias',$categorias);
-          // }
-
-
-        //});
     }
 }
