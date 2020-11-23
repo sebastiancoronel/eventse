4c14ed25edf10f3f5d5afcea35caba9251c44220
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2020-11-23 12:54:06 -0300

    Delete de servicio por ajax y reacomodo del form

--- resources/views/Principal/carrito.blade.php
+++ resources/views/Principal/carrito.blade.php
@@ -1,352 +1,156 @@
 @extends('Principal.Partials.master')
 @section('content')
 <div class="animsition">
-<!-- Carrito escritorio -->
 	<div class="bg0 dispositivo">
 		<div class="container">
-			<div class="row">
-				<div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
-					<div class="m-l-25 m-r--38 m-lr-0-xl">
-						<!-- Item 1 -->
-						@if ($Paquete)
-							@foreach ($Paquete as $servicio)
-							<form action=" {{ route('EliminarDelPaquete') }} " method="POST">
-								@csrf 
-								<input type="text" name="id_servicio" class="d-none" value="{{ $servicio['id_servicio'] }}">
-								<div class="row container">
-									{{-- <div class="col-md-3">
-										<img class="img-fluid" src="{{asset('images/castillo2.webp')}}" alt="IMG">
-									</div> --}}
-									<div class="row col-md-6">
-										<span class="col-md-12"> {{ $servicio['NombreServicio'] }} </span>
-										<a class="col-md-12 text-primary mt-2"><small>Agregar un comentario</small></a>
-									</div>
-									<div class="col-md-3">
-										@if ( $servicio['Precio'] != 'Precio a convenir' )
-											$ {{ $servicio['Precio'] }}
-										@else
-											{{ $servicio['Precio'] }}
-										@endif
-										<span>
-											{{-- <small class="text-success">Presupuestado</small> --}}
-										</span>
-									</div>
+			<form action=" {{ route('EnviarSolicitudPresupuesto') }} " method="POST">
+				@csrf 
+				<div class="row">
+					{{-- Servicios del paquete --}}
+					<div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
+						<div class="m-l-25 m-r--38 m-lr-0-xl">
+							<!-- Item 1 -->
+							@if ($Paquete)
+								@foreach ($Paquete as $servicio)
+									<div class="row container">
+										{{-- <div class="col-md-3">
+											<img class="img-fluid" src="{{asset('images/castillo2.webp')}}" alt="IMG">
+										</div> --}}
+										<div class="row col-md-6">
+											<span class="col-md-12"> {{ $servicio['NombreServicio'] }} </span>
+												<small class="col-md-12 text-primary mt-2">Agregar un comentario</small>
+										</div>
+										<div class="col-md-3">
+											@if ( $servicio['Precio'] != 'Precio a convenir' )
+												$ {{ $servicio['Precio'] }}
+											@else
+												{{ $servicio['Precio'] }}
+											@endif
+											<span>
+												{{-- <small class="text-success">Presupuestado</small> --}}
+											</span>
+										</div>
 
-									<div class="col-md-3">
-										<button type="submit" class="flex-c-m stext-101 cl2 size-100 bg8 bor13 hov-btn4 p-lr-15 trans-04 pointer m-tb-5">
-											Eliminar
-										</button>
-									</div>
-								</div>
-							</form>
-								<hr>
-							@endforeach
-						@else
-						<span class=""> No tienes servicios en tu paquete aún </span>
-						@endif
-					</div>
-				</div>
-				
-				@if ($Paquete)
-					<div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
-						<div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
-							<h4 class="mtext-109 cl2 p-b-30">
-								Datos de traslado
-							</h4>
-
-							{{-- <div class="flex-w flex-t bor12 p-b-13">
-								<div class="size-208">
-									<span class="stext-110 cl2">
-										Subtotal:
-									</span>
-								</div>
-
-								<div class="size-209">
-									<span class="mtext-110 cl2">
-										$79.65
-									</span>
-								</div>
-							</div> --}}
-							<div class="flex-w flex-t bor12 p-t-15 p-b-30">
-								<div class="size-208 w-full-ssm">
-									<span class="stext-110 cl2">
-										Envío:
-									</span>
-								</div>
-
-								<div class="size-209 p-r-18 p-r-0-sm w-full-ssm">
-									<p class="stext-111 cl6 p-t-2">
-										Indicanos la direccion del evento
-									</p>
-
-									<div class="p-t-15">
-										{{-- <span class="stext-112 cl8">
-											Calcular envío
-										</span> --}}
-										<div class="form-group">
-											<span>{{ Auth::user()->provincia }} , {{ Auth::user()->localidad }} </span>
+										<div class="col-md-3" data-id="{{ $servicio['id_servicio'] }}">
+											<button type="button" class="flex-c-m stext-101 cl2 size-100 bg8 bor13 hov-btn4 p-lr-15 trans-04 pointer m-tb-5 eliminar-del-paquete">
+												Eliminar
+											</button>
 										</div>
 
 										<div class="bor8 bg0 m-b-12">
-											<textarea class="stext-111 cl8 plh3 size-111 p-lr-15" name="direccion" maxlength="100" id="" cols="30" rows="10">
-
-											</textarea>
+											<textarea class="stext-111 cl8 plh3 size-111 p-lr-15 h-100" name="comentario_adicional[]" maxlength="500" cols="100" rows="5"></textarea>
 										</div>
-
-										<div class="bor8 bg0 m-b-22">
-											<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="barrio" maxlength="100" placeholder="Barrio">
-										</div>
-
-										{{-- <div class="flex-w">
-											<div class="flex-c-m stext-101 cl2 size-115 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer">
-												Actualizar total
-											</div>
-										</div> --}}
-
 									</div>
-								</div>
-							</div>
-								
-								{{-- <div class="flex-w flex-t p-t-27 p-b-33">
-								<div class="size-208">
-									<span class="mtext-101 cl2">
-										Total:
-									</span>
-								</div>
-								
-								<div class="size-209 p-t-1">
-									<span class="mtext-110 cl2"style="color:#f40082;">
-										<strong>$79.65</strong>
-									</span>
-								</div>
-							</div> --}}
-							<button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
-								Enviar solicitud
-							</button>
+									<hr>
+								@endforeach
+							@else
+							<span class=""> No tienes servicios en tu paquete aún </span>
+							@endif
 						</div>
 					</div>
-				@endif
-			</div>
+					
+					{{-- Envío --}}
+					@if ($Paquete)
+						<div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
+							<div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
+								<h4 class="mtext-109 cl2 p-b-30">
+									Datos de traslado
+								</h4>
+								{{-- <div class="flex-w flex-t bor12 p-b-13">
+									<div class="size-208">
+										<span class="stext-110 cl2">
+											Subtotal:
+										</span>
+									</div>
+
+									<div class="size-209">
+										<span class="mtext-110 cl2">
+											$79.65
+										</span>
+									</div>
+								</div> --}}
+								<div class="flex-w flex-t bor12 p-t-15 p-b-30">
+									<div class="size-208 w-full-ssm">
+										<span class="stext-110 cl2">
+											Envío:
+										</span>
+									</div>
+
+									<div class="size-209 p-r-18 p-r-0-sm w-full-ssm">
+										<p class="stext-111 cl6 p-t-2">
+											Indicanos la direccion para enviar los servicios que requieran traslado
+										</p>
+
+										<div class="p-t-15">
+											{{-- <span class="stext-112 cl8">
+												Calcular envío
+											</span> --}}
+											<div class="form-group">
+												<span>{{ Auth::user()->provincia }} , {{ Auth::user()->localidad }} </span>
+											</div>
+
+											<div class="bor8 bg0 m-b-12">
+												<textarea class="stext-111 cl8 plh3 size-111 p-lr-15" name="direccion" maxlength="100" id="" cols="30" rows="10" placeholder="Dirección"></textarea>
+											</div>
+
+											<div class="bor8 bg0 m-b-22">
+												<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="barrio" maxlength="100" placeholder="Barrio">
+											</div>
+
+											{{-- <div class="flex-w">
+												<div class="flex-c-m stext-101 cl2 size-115 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer">
+													Actualizar total
+												</div>
+											</div> --}}
+
+										</div>
+									</div>
+								</div>
+									
+									{{-- <div class="flex-w flex-t p-t-27 p-b-33">
+									<div class="size-208">
+										<span class="mtext-101 cl2">
+											Total:
+										</span>
+									</div>
+									
+									<div class="size-209 p-t-1">
+										<span class="mtext-110 cl2"style="color:#f40082;">
+											<strong>$79.65</strong>
+										</span>
+									</div>
+								</div> --}}
+								<button type="submit" class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
+									Enviar solicitud
+								</button>
+							</div>
+						</div>
+					@endif
+				</div>
+			</form>
 		</div>
 	</div>
-<!-- Fin Carrito escritorio -->
-
-<!-- Carrito movil -->
-  {{-- <form class="bg0 mt-3 d-md-none d-lg-none d-xl-none d-xs-block d-sm-block">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
-          <div class="m-l-25 m-r--38 m-lr-0-xl">
-            <div class="wrap-table-shopping-cart">
-
-              <table>
-                <tr class="table_row">
-									<!-- Botón Elipsis esquina superior derecha -->
-										<div class="h-100 d-flex justify-content-end">
-											<a class="nav-link" data-toggle="dropdown" href="#">
-												<i class="d-md-none d-lg-none d-xl-none d-xs-block d-sm-block text-muted fas fa-ellipsis-v text-muted" style="cursor: pointer;"></i>
-											</a>
-											<!-- Menu Dropdown -->
-										 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-											<!-- Item -->
-											<a href="#" class="dropdown-item">
-												<div class="media">
-													<div class="media-body">
-														<h3 class="dropdown-item-title text-left">Ver</h3>
-													</div>
-												</div>
-											</a>
-											<div class="dropdown-divider"></div>
-											<!-- Fin Item -->
-
-											 <!-- Item -->
-											 <a href="#" class="dropdown-item bg-danger">
-												<div class="media">
-													<div class="media-body">
-														<h3 class="dropdown-item-title text-left">Eliminar</h3>
-													</div>
-												</div>
-											</a>
-											<div class="dropdown-divider"></div>
-											<!-- Fin Item -->
-
-										</div>
-										<!-- Fin Menu Dropdown -->
-										</div>
-									<!-- Fin Botón Elipsis esquina superior derecha -->
-                  <td class="column-1">
-                    <div class="how-itemcart1">
-                      <img src="{{asset('images/castillo2.webp')}}" alt="IMG">
-                    </div>
-                  </td>
-                  <td class="column-2">Castillo inflable de 3x3mts</td>
-                  <td class="column-3 ml-5 pl-5">$600.00</td>
-                </tr>
-
-								<table class="mt-4 d-flex justify-content-center">
-									<tr class="table_row row">
-										<td> <a href="#">Ver más servicios del prestador</a></td>
-									</tr>
-								</table>
-							</table>
-
-							<hr>
-
-							<table>
-                <tr class="table_row">
-									<!-- Botón Elipsis esquina superior derecha -->
-										<div class="h-100 d-flex justify-content-end">
-											<a class="nav-link" data-toggle="dropdown" href="#">
-												<i class="d-md-none d-lg-none d-xl-none d-xs-block d-sm-block text-muted fas fa-ellipsis-v text-muted" style="cursor: pointer;"></i>
-											</a>
-											<!-- Menu Dropdown -->
-										 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-											<!-- Item -->
-											<a href="#" class="dropdown-item">
-												<div class="media">
-													<div class="media-body">
-														<h3 class="dropdown-item-title text-left">Ver</h3>
-													</div>
-												</div>
-											</a>
-											<div class="dropdown-divider"></div>
-											<!-- Fin Item -->
-
-											 <!-- Item -->
-											 <a href="#" class="dropdown-item bg-danger">
-												<div class="media">
-													<div class="media-body">
-														<h3 class="dropdown-item-title text-left">Eliminar</h3>
-													</div>
-												</div>
-											</a>
-											<div class="dropdown-divider"></div>
-											<!-- Fin Item -->
-
-										</div>
-										<!-- Fin Menu Dropdown -->
-										</div>
-									<!-- Fin Botón Elipsis esquina superior derecha -->
-                  <td class="column-1">
-                    <div class="how-itemcart1">
-                      <img src="{{asset('images/castillo1.webp')}}" alt="IMG">
-                    </div>
-                  </td>
-                  <td class="column-2">Castillo inflable de 4x3mts</td>
-                  <td class="column-3 ml-5 pl-5">Precio a convenir</td>
-                </tr>
-							</table>
-
-							<table class="mt-4 d-flex justify-content-center">
-								<tr class="table_row row">
-									<td> <a href="#">Ver más servicios del prestador</a></td>
-								</tr>
-							</table>
-						</table>
-            </div>
-
-            <div class="flex-w flex-sb-m bor15 p-t-18 p-b-15 p-lr-40 p-lr-15-sm">
-              <div class="flex-w flex-m m-r-20 m-tb-5">
-                <input class="stext-104 cl2 plh4 size-117 bor13 p-lr-20 m-r-10 m-tb-5" type="text" name="coupon" placeholder="Código del cupón">
-
-                <div class="flex-c-m stext-101 cl2 size-118 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-5">
-                  Aplicar cupón
-                </div>
-              </div>
-
-              <div class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
-                Actualizar paquete
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
-          <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
-            <h4 class="mtext-109 cl2 p-b-30">
-              Total del paquete
-            </h4>
-
-            <div class="flex-w flex-t bor12 p-b-13">
-              <div class="size-208">
-                <span class="stext-110 cl2">
-                  Subtotal:
-                </span>
-              </div>
-
-              <div class="size-209">
-                <span class="mtext-110 cl2">
-                  $79.65
-                </span>
-              </div>
-            </div>
-
-            <div class="flex-w flex-t bor12 p-t-15 p-b-30">
-              <div class="size-208 w-full-ssm">
-                <span class="stext-110 cl2">
-                  Envío:
-                </span>
-              </div>
-
-              <div class="size-209 p-r-18 p-r-0-sm w-full-ssm">
-                <p class="stext-111 cl6 p-t-2">
-                  There are no shipping methods available. Please double check your address, or contact us if you need any help.
-                </p>
-
-                <div class="p-t-15">
-                  <span class="stext-112 cl8">
-                    Calcular envío
-                  </span>
-
-                  <div class="rs1-select2 rs2-select2 bor8 bg0 m-b-12 m-t-9">
-                    <select class="js-select2" name="time">
-                      <option>Seleccionar una localidad...</option>
-                      <option>Santiago del Estero</option>
-                      <option>La Banda</option>
-                    </select>
-                    <div class="dropDownSelect2"></div>
-                  </div>
-
-                  <div class="bor8 bg0 m-b-12">
-                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="state" placeholder="State /  country">
-                  </div>
-
-                  <div class="bor8 bg0 m-b-22">
-                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="postcode" placeholder="Postcode / Zip">
-                  </div>
-
-                  <div class="flex-w">
-                    <div class="flex-c-m stext-101 cl2 size-115 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer">
-                      Actualizar total
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-            </div>
-
-            <div class="flex-w flex-t p-t-27 p-b-33">
-              <div class="size-208">
-                <span class="mtext-101 cl2">
-                  Total:
-                </span>
-              </div>
-
-              <div class="size-209 p-t-1">
-                <span class="mtext-110 cl2"style="color:#f40082;">
-                <strong>$79.65</strong>
-                </span>
-              </div>
-            </div>
-
-            <button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
-              Proceder a pagar el paquete
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </form> --}}
-<!-- Fin carrito movil -->
 </div>
+
+@push('js')
+	<script>
+		$(document).on('click', '.eliminar-del-paquete', function(){
+			id_servicio = $(this).parents().data('id');
+			$.ajax({
+				method: 'POST',
+				url: '{{ url('/reservar/servicios-publicados/eliminando-del-paquete') }}',
+				data:{
+					_token: '{{ csrf_token() }}',
+					id_servicio
+				},
+				error: function(x,y,z){
+					console.log(x,y,z);
+				},
+				success: function(data){
+					location.reload();
+				}
+			});
+		});
+	</script>
+@endpush
 @endsection
