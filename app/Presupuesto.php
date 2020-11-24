141cc300366638d9821c67a0c350bec1bd827d54
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2020-11-24 17:11:30 -0300

    Se completo el modelo

--- app/Presupuesto.php
+++ app/Presupuesto.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Presupuesto extends Model
 {
-    //
+    protected $table = 'presupuestos';
+    protected $fillable = ['id_servicio','id_prestador','user_id','fecha','hora_desde','hora_hasta','direccion','barrio','monto','estado','pregunta','respuesta'];
 }
