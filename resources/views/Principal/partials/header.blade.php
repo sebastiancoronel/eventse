b6d8590e682a7aaa515faef30ef692a2a711e1e0
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2020-12-17 22:00:25 -0300

    Se oculto lupa para buscar

--- resources/views/Principal/partials/header.blade.php
+++ resources/views/Principal/partials/header.blade.php
@@ -87,9 +87,9 @@
 
                 <!-- Icon header - Buscar - Carrito de compras - Favoritos -->
                 <div class="wrap-icon-header flex-w flex-r-m">
-                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+                    {{-- <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
                         <i class="zmdi zmdi-search"></i>
-                    </div>
+                    </div> --}}
               {{-- Carrito --}}
               <div id="carrito_escritorio" class="icon-header-item icon-header-noti cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-cart" data-notify="{{ $CantidadServicios }}"> 
                   <i class="zmdi zmdi-shopping-cart"></i>
