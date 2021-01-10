7c7ebd20957a2892ab7d919f6dfe200c01f224f8
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2021-01-10 19:58:04 -0300

    Cargar mas comentado

--- resources/views/Principal/principal.blade.php
+++ resources/views/Principal/principal.blade.php
@@ -339,11 +339,11 @@
             </div> <!-- row isotope-grid -->
 
 			<!-- Load more -->
-			<div class="flex-c-m flex-w w-full p-t-45">
+			{{-- <div class="flex-c-m flex-w w-full p-t-45">
 				<a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
 					Cargar más
 				</a>
-			</div>
+			</div> --}}
 		</div>
 	</section>
 
