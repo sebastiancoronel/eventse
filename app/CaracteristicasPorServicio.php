b11454850389fb65798700e2a348380b65077920
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2021-02-13 18:45:14 -0300

    Sin softDelete para evitar que se llene al pedo

--- app/CaracteristicasPorServicio.php
+++ app/CaracteristicasPorServicio.php
@@ -3,10 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 class CaracteristicasPorServicio extends Model
 {
-    use SoftDeletes;
+    // use SoftDeletes;
 
     protected $table = 'caracteristicas_por_servicios';
     protected $fillable = [ 'id_servicio', 'id_caracteristica' ];
