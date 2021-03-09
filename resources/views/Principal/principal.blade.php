3657974f4a908629a6f519855244eda941156a9d
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2021-03-09 18:36:43 -0300

    Nuevo formato para destacados

--- resources/views/Principal/principal.blade.php
+++ resources/views/Principal/principal.blade.php
@@ -128,8 +128,6 @@
 	</section>
 	<!--Fin Slider-->
 
-	
-
 	<!--SELECCION DE CATEGORIAS-->
 	{{-- <div class="sec-banner bg0 p-b-50 mt-5">
 		<div class="container">
@@ -164,11 +162,9 @@
 		</div>
 	</div> --}}
 	<!--Fin SELECCION DE CATEGORIAS-->
-
-	
-	
+	<hr>
 	<div class="container my-5">
-		<div class="p-b-10">
+		<div class="p-b-10 text-center">
 			<h3 class="ltext-103 cl5">
 				Últimos publicados
 			</h3>
@@ -210,197 +206,88 @@
 	</div>
 	<!--Fin Servicios-->
 
-	<!-- Product -->
+	<hr>
+	<!-- Destacados -->
 	<section class="bg0 p-t-23 p-b-140">
 		<div class="container">
-			<div class="p-b-10">
+			<div class="p-b-10 text-center">
 				<h3 class="ltext-103 cl5">
-					Filtrar
+					Destacados
 				</h3>
 			</div>
 
-			<div class="flex-w flex-sb-m p-b-52">
-				<div class="flex-w flex-l-m filter-tope-group m-tb-10">
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
-						Todos los productos
-					</button>
-
-					@foreach ($Categorias as $categoria)
-                    <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$categoria->nombre}}">
-                        {{$categoria->nombre}}
-                    </button>
-                    @endforeach
-				</div>
-
-				<div class="flex-w flex-c-m m-tb-10">
-					<div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
-						<i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
-						<i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-						Filtrar
-					</div>
-
-					<div class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
-						<i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
-						<i class="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-						Buscar
-					</div>
-				</div>
-
-				<!-- Search product -->
-				<div class="dis-none panel-search w-full p-t-10 p-b-15">
-					<div class="bor8 dis-flex p-l-15">
-						<button class="size-113 flex-c-m fs-16 cl2 hov-cl1 trans-04">
-							<i class="zmdi zmdi-search"></i>
-						</button>
-
-						<input class="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="search-product" placeholder="Search">
-					</div>
-				</div>
-
-				<!-- Filter -->
-				<div class="dis-none panel-filter w-full p-t-10">
-					<div class="wrap-filter flex-w bg6 w-full p-lr-40 p-t-27 p-lr-15-sm">
-						<div class="filter-col1 p-r-15 p-b-27">
-							<div class="mtext-102 cl2 p-b-15">
-								Ordenar por
-							</div>
-
-							<ul>
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										Default
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										Popularity
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										Average rating
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04 filter-link-active">
-										Newness
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										Price: Low to High
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										Price: High to Low
-									</a>
-								</li>
-							</ul>
-						</div>
-
-						<div class="filter-col2 p-r-15 p-b-27">
-							<div class="mtext-102 cl2 p-b-15">
-								Price
-							</div>
-
-							<ul>
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04 filter-link-active">
-										All
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										$0.00 - $50.00
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										$50.00 - $100.00
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										$100.00 - $150.00
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										$150.00 - $200.00
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04">
-										$200.00+
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div id="" class="row isotope-grid" style="position: relative; height: 1717.38px;">
+			<div id="" class="row isotope-grid mt-5" style="position: relative; height: 1717.38px;">
                 @foreach ($FiltrarServicios as $filtrar_servicio)
                     <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item {{$filtrar_servicio->nombre_categoria}}" style="position: absolute; left: 0%; top: 0px;">
                         <!-- Block2 -->
-                        <div class="block2">
-                        <div class="block2-pic hov-img0">
-                            <img src="{{ asset($filtrar_servicio->foto_1) }}" alt="IMG-PRODUCT">
+                        <div class="block2 bor10">
+							<div class="block2-pic hov-img0">
+								<div style="background-image: url({{ asset($filtrar_servicio->foto_1) }}); background-position: center; background-size: cover; height: 200px;"> </div>
+								{{-- <img src="{{ asset($filtrar_servicio->foto_1) }}" alt="IMG-PRODUCT"> --}}
 
-                            <a href=" {{route('MostrarServicio',[ 'id' => $filtrar_servicio  ])}} " class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04">
-                            Ver servicio
-                            </a>
-                        </div>
+								<a href=" {{route('MostrarServicio',[ 'id' => $filtrar_servicio  ])}} " class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04">
+								Ver servicio
+								</a>
+							</div>
 
-                        <div class="block2-txt flex-w flex-t p-t-14">
-                            <div class="block2-txt-child1 flex-col-l ">
-                            <a href="{{route('MostrarServicio',[ 'id' => $filtrar_servicio  ])}}" class="text-left cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                {{$filtrar_servicio->nombre}}
-                            </a>
+							<div class="block2-txt flex-w flex-t p-t-14 mx-3">
+								<div class="row">
+									<div class="col-lg-12 col-12 text-center">
+										<a href="{{route('MostrarServicio',[ 'id' => $filtrar_servicio  ])}}" class="text-left cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+											{{ \Illuminate\Support\Str::limit($filtrar_servicio->nombre, 30) }}
+										</a>
+									</div>
 
-                            <span class="stext-105 cl3">
-                                @if ($filtrar_servicio->precio != null)
-                                $ {{$filtrar_servicio->precio}}
-                                @else
-                                Precio a convenir
-                                @endif
-                            </span>
-                            <span class="mt-2">
-                                <i class="zmdi zmdi-pin"></i> <small> {{ $filtrar_servicio->localidad }} , {{ $filtrar_servicio->provincia }} </small>
-                            </span>
-                            </div>
+									<div class="col-lg-12 col-12 text-center my-2">
+										<span class="stext-105 cl3">
+											@if ($filtrar_servicio->precio != null)
+											$ {{$filtrar_servicio->precio}}
+											@else
+											Precio a convenir
+											@endif
+										</span>
+									</div>
 
-                            {{-- <div class="block2-txt-child2 flex-r p-t-3">
-                                <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                    <img class="icon-heart1 dis-block trans-04" src="{{asset('images/icons/icon-heart-01.png')}}" alt="ICON">
-                                    <img class="icon-heart2 dis-block trans-04 ab-t-l" src="{{asset('images/icons/icon-heart-02.png')}}" alt="ICON">
-                                </a>
-                            </div> --}}
-                        </div>
+									<div class="col-lg-12 col-12 text-center">
+										<span class="mt-2">
+											<i class="zmdi zmdi-pin"></i> <small> {{ $filtrar_servicio->localidad }} , {{ $filtrar_servicio->provincia }} </small>
+										</span>
+									</div>
+									
+								</div>
+
+								<!-- Separador -->
+								<div class="bor10 mt-3" style="width: 100%; height: 1px;"></div>
+
+								<div class="text-center mt-3">
+									<span>
+										<i class="zmdi zmdi-star text-warning"></i>
+										<i class="zmdi zmdi-star text-warning"></i>
+										<i class="zmdi zmdi-star text-warning"></i>
+										<i class="zmdi zmdi-star text-warning"></i>
+										<i class="zmdi zmdi-star text-warning"></i>
+										{{-- Pienso establecer estrellas segun cantidad de contrataciones exitosas
+											+10 1 estrella +20 2 estrellas asi hasta +50 5 estrellas --}}
+										+100 contrataciones
+									</span>
+								</div>
+
+								<!-- Separador -->
+								<div class="bor10 mt-3" style="width: 100%; height: 1px;"></div>
+
+								<div class="text-center mt-3">
+									<small>
+										{{ \Illuminate\Support\Str::limit($filtrar_servicio->descripcion, 80) }}
+									</small>
+								</div>
+
+							</div>
                         </div>
                     </div>
                 @endforeach
                
             </div> <!-- row isotope-grid -->
 
-			<!-- Load more -->
-			{{-- <div class="flex-c-m flex-w w-full p-t-45">
-				<a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
-					Cargar más
-				</a>
-			</div> --}}
 		</div>
 	</section>
 
