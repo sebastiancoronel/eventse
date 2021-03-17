3e8be1d03532e3359cee34ce2616a3a8927aa278
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2021-03-17 13:07:42 -0300

    Comente lineas que no se usan

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,29 +12,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        DB::table('provincias')->insert(['nombre' => 'Buenos Aires']);
-        DB::table('provincias')->insert(['nombre' => 'Catamarca']);
-        DB::table('provincias')->insert(['nombre' => 'Chaco']);
-        DB::table('provincias')->insert(['nombre' => 'Chubut']);
-        DB::table('provincias')->insert(['nombre' => 'Córdoba']);
-        DB::table('provincias')->insert(['nombre' => 'Corrientes']);
-        DB::table('provincias')->insert(['nombre' => 'Entre Ríos']);
-        DB::table('provincias')->insert(['nombre' => 'Formosa']);
-        DB::table('provincias')->insert(['nombre' => 'Jujuy']);
-        DB::table('provincias')->insert(['nombre' => 'La Pampa']);
-        DB::table('provincias')->insert(['nombre' => 'La Rioja']);
-        DB::table('provincias')->insert(['nombre' => 'Mendoza']);
-        DB::table('provincias')->insert(['nombre' => 'Misiones']);
-        DB::table('provincias')->insert(['nombre' => 'Neuquén']);
-        DB::table('provincias')->insert(['nombre' => 'Río Negro']);
-        DB::table('provincias')->insert(['nombre' => 'Salta']);
-        DB::table('provincias')->insert(['nombre' => 'San Juan']);
-        DB::table('provincias')->insert(['nombre' => 'San Luis']);
-        DB::table('provincias')->insert(['nombre' => 'Santa Cruz']);
-        DB::table('provincias')->insert(['nombre' => 'Santa Fe']);
-        DB::table('provincias')->insert(['nombre' => 'Santiago del Estero']);
-        DB::table('provincias')->insert(['nombre' => 'Tierra del Fuego']);
-        DB::table('provincias')->insert(['nombre' => 'Tucumán']);
+        // DB::table('provincias')->insert(['nombre' => 'Buenos Aires']);
+        // DB::table('provincias')->insert(['nombre' => 'Catamarca']);
+        // DB::table('provincias')->insert(['nombre' => 'Chaco']);
+        // DB::table('provincias')->insert(['nombre' => 'Chubut']);
+        // DB::table('provincias')->insert(['nombre' => 'Córdoba']);
+        // DB::table('provincias')->insert(['nombre' => 'Corrientes']);
+        // DB::table('provincias')->insert(['nombre' => 'Entre Ríos']);
+        // DB::table('provincias')->insert(['nombre' => 'Formosa']);
+        // DB::table('provincias')->insert(['nombre' => 'Jujuy']);
+        // DB::table('provincias')->insert(['nombre' => 'La Pampa']);
+        // DB::table('provincias')->insert(['nombre' => 'La Rioja']);
+        // DB::table('provincias')->insert(['nombre' => 'Mendoza']);
+        // DB::table('provincias')->insert(['nombre' => 'Misiones']);
+        // DB::table('provincias')->insert(['nombre' => 'Neuquén']);
+        // DB::table('provincias')->insert(['nombre' => 'Río Negro']);
+        // DB::table('provincias')->insert(['nombre' => 'Salta']);
+        // DB::table('provincias')->insert(['nombre' => 'San Juan']);
+        // DB::table('provincias')->insert(['nombre' => 'San Luis']);
+        // DB::table('provincias')->insert(['nombre' => 'Santa Cruz']);
+        // DB::table('provincias')->insert(['nombre' => 'Santa Fe']);
+        // DB::table('provincias')->insert(['nombre' => 'Santiago del Estero']);
+        // DB::table('provincias')->insert(['nombre' => 'Tierra del Fuego']);
+        // DB::table('provincias')->insert(['nombre' => 'Tucumán']);
     }
     
 }
