b031797faadd822943b1d82ebb91f09b50176508
DESKTOP-GS1KJCT\Sebastian <seba_c94@hotmail.com>
2021-03-31 16:57:44 -0300

    Modificacion reservar por ver servicio

--- resources/views/Principal/servicios_publicados.blade.php
+++ resources/views/Principal/servicios_publicados.blade.php
@@ -402,7 +402,7 @@
                                         <img src="`+ foto +`" alt="IMG-PRODUCT">
 
                                         <a href="/reservar/servicios-publicados/`+ value.id +`" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04">
-                                        Reservar
+                                        Ver servicio
                                         </a>
                                     </div>
 
